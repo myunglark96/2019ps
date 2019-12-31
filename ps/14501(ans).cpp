0ccdc47a816ea4da3e7e58f538e2cdeb0435d44b
myunglark@gmail.com <myunglark@gmail.com>
2019-12-31 21:35:15 +0900

    [Ans] add 14501 dp ans

--- ps/14501(ans).cpp
+++ ps/14501(ans).cpp
@@ -1,3 +1,4 @@
+//dfs를 이용한 풀이
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -13,8 +14,6 @@ void problemIn() {
         cin>>day[i]>>price[i];
     }
 }
-
-
 
 void dfs(int curD, int sum) {
 
@@ -46,3 +45,46 @@ int main() {
     cout<<maxVal<<endl;
     return 0;
 }
+
+//dp를 이용한 풀이
+/*
+#include <iostream>
+
+using namespace std;
+
+int t[1000] = { 0, };
+int p[1000] = { 0, };
+int dp[1000] = { 0, };
+
+int max(int x, int y)
+{
+    return x > y ? x : y;
+}
+
+int main()
+{
+    int N;
+    int next;
+
+    cin >> N;
+
+    for (int i = 1;i <= N;i++) {
+        cin >> t[i] >> p[i];
+    }
+
+    for (int i = N;i > 0;i--) {
+        next = i + t[i];
+        if (next > N + 1) {
+            dp[i] = dp[i + 1];
+        }
+        else {
+            dp[i] = max(dp[i + 1], dp[next] + p[i]);
+        }
+
+    }
+
+    cout << dp[1] << endl;
+
+    return 0;
+}
+*/
